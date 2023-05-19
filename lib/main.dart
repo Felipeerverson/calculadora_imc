f45040f275c58bf907335cdde8a07bb8ad7bc730
Felipe Erverson <107085822+Felipeerverson@users.noreply.github.com>
2023-05-19 15:20:03 -0300

    Projeto de Calculadora IMC.

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _HomeState extends State<Home> {
         actions: [
           IconButton(
             onPressed: _resetFields,
-            icon: Icon(Icons.refresh) ,
+            icon: Icon(Icons.refresh),
           ),
         ],
       ),
